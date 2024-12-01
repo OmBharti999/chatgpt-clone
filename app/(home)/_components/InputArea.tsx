376e6f8e200b64e3f8215cbd69ef61f07698d4ee
Om Bharti <ombharti888@gmail.com>
2024-12-01 22:37:57 +0530

    Fix layout bug

--- app/(home)/_components/InputArea.tsx
+++ app/(home)/_components/InputArea.tsx
@@ -1,15 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { useChatContext } from "@/app/_contexts/ChatContext";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Send } from "lucide-react";
 
 export const InputArea = () => {
+  const { setChat } = useChatContext();
+  const [inputValue, setInputValue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      setChat((prevChat) => [
+        ...prevChat,
+        {
+          author: "Human",
+          content: inputValue.trim(),
+          id: `${prevChat.length}`,
+        },
+      ]);
+      setInputValue("");
+    }
+  };
+
   return (
     <div className="border-t dark:border-gray-700 p-4">
-      <form className="flex space-x-2">
+      <form className="flex space-x-2" onSubmit={onSubmit}>
         <Input
           className="flex-1"
           placeholder="Type your message here..."
           type="text"
+          name="prompt"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <Button type="submit" size="icon">
           <Send className="h-4 w-4" />
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { ChatArea, Header, Sidebar, InputArea } from "./_components";
+import { ChatArea, InputArea } from "./_components";
 
 export default function Home() {
   return (
