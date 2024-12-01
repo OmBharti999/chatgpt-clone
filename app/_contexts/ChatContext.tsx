15468fdaa5a9e5a098f25a4f847a6ce566a6e381
Om <dev@grippi.io>
2024-12-01 22:09:13 +0530

    add ChatContextProvider

---
+++ app/_contexts/ChatContext.tsx
@@ -0,0 +1,35 @@
+import { useContext, createContext, useState, Dispatch, SetStateAction, ReactNode } from "react";
+
+type Author = "AI" | "Human";
+
+interface Chat {
+  id: string;
+  content: string;
+  author: Author;
+}
+
+interface ChatContextType {
+  chat: Chat[];
+  setChat: Dispatch<SetStateAction<Chat[]>>;
+}
+
+const defaultValue: ChatContextType = {
+  chat: [],
+  setChat: () => {},
+};
+
+const ChatContext = createContext<ChatContextType>(defaultValue);
+
+export const ChatContextProvider = ({ children }: { children: ReactNode }) => {
+  const [chat, setChat] = useState<Chat[]>([]);
+  return (
+    <ChatContext.Provider value={{ chat, setChat }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChatContext = () => {
+  return useContext(ChatContext);
+};
+
